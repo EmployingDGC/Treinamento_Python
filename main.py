e023525f7bceab0cfe6fed7840aadab3d0bdde47
EmployingDGC <daviguizani@gmail.com>
2021-05-24 15:26:09 -0300

    organização exercícios parte 3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pandas
+import pandas as pd
 
 
-def convert_column_to_float64(column_data_frame: pandas.DataFrame,
-                              default: float) -> pandas.DataFrame:
+def convert_column_to_float64(column_data_frame: pd.DataFrame,
+                              default: float) -> pd.DataFrame:
     return column_data_frame.apply(
         lambda num:
         num
@@ -13,8 +13,8 @@ def convert_column_to_float64(column_data_frame: pandas.DataFrame,
     ).astype("float64")
 
 
-def convert_column_to_int64(column_data_frame: pandas.DataFrame,
-                            default: int) -> pandas.DataFrame:
+def convert_column_to_int64(column_data_frame: pd.DataFrame,
+                            default: int) -> pd.DataFrame:
     return column_data_frame.apply(
         lambda num:
         num
@@ -29,31 +29,43 @@ if __name__ == '__main__':
 
     filename = "./Datasets/Imóveis.csv"
 
-    frame = pandas.read_csv(filename, sep=",")
+    # exercício 1
+    frame = pd.read_csv(filename, sep=",")
 
-    print(f"\n{frame.shape}")
+    # exercício 2
+    print(f"\n# exercício 2:\n{frame.shape}")
 
-    print(f"\n{frame.head(10)}")
+    # exercício 3
+    print(f"\n# exercício 3:\n{frame.head(10)}")
 
-    print(f"\n{frame.columns}")
+    # exercício 4
+    print(f"\n# Exercício 4:\n{frame.columns}")
 
-    print(f"\n{frame.dtypes}")
+    # exercício 5
+    print(f"\n# exercício 5:\n{frame.dtypes}")
 
-    frame["data"] = pandas.to_datetime(frame["data"])
+    # exercício 6
+    frame["data"] = pd.to_datetime(frame["data"])
 
+    # exercício 7
     frame["análise"] = 2021
 
-    print(f"\n{pandas.DataFrame(frame, columns=['id', 'data', 'preço', 'quartos', 'banheiros', 'ano de construção'])}")
+    # exercício 8
+    print(f"\n# exercício 8:\n{pd.DataFrame(frame, columns=['id', 'data', 'preço', 'quartos', 'banheiros', 'ano de construção'])}")
 
+    # exercício 9
     frame.drop(["metragem da casa.1"], axis=1)  # axis começa em 0, main axis = linha
 
+    # exercício 10
     frame["banheiros"] = frame["banheiros"].apply(
         lambda num:
         str(num).split(",")[0]
     ).astype('int64')
 
-    print(f"\n{frame.loc[frame['ano de construção'] < 2000]}")
+    # exercício 11
+    print(f"\n# exercício 11:\n{frame.loc[frame['ano de construção'] < 2000]}")
 
+    # exercício 12
     frame = frame.rename(columns={
         "data": "DT_REFERENCIA",
         "preço": "VL_PRECO",
@@ -70,6 +82,7 @@ if __name__ == '__main__':
 
     print(f"\n{frame.dtypes}")
 
+    # exercício 13
     frame["DS_TIPO_CONDICAO"] = [
         "Ruim"
         if num < 2
@@ -87,39 +100,47 @@ if __name__ == '__main__':
         str(num).replace(",", ".")
     ).astype('float64')
 
-    print(f"\n{frame[['id', 'VL_PRECO']].sort_values(['VL_PRECO'])}")
+    # exercício 14
+    print(f"\n# exercício 14:\n{frame[['id', 'VL_PRECO']].sort_values(['VL_PRECO'])}")
 
+    # exercício 15
     frame_filtro_1 = frame.query("DS_TIPO_CONDICAO == 'Regular'")
 
-    print(f"\n{frame_filtro_1.shape[0]}")
+    print(f"\n# exercício 15:\n{frame_filtro_1.shape[0]}")
 
-    print(f"\n{frame_filtro_1['VL_PRECO'].mean()}")
+    # exercício 16
+    print(f"\n# exercício 16:\n{frame_filtro_1['VL_PRECO'].mean()}")
 
+    # exercício 17
     frame_filtro_2 = frame.query("QTD_QUARTOS == 3")
 
     preco_max = frame_filtro_2['VL_PRECO'].max()
 
-    print(f"\n{preco_max}")
+    print(f"\n# exercício 17:\n{preco_max}")
 
+    # exercício 18
     frame_filtro_3 = frame.query(
         "DS_TIPO_CONDICAO == 'Bom' and QTD_QUARTOS > 2 and QTD_BANHEIROS > 2"
     )
 
-    print(f"\n{frame_filtro_3}")
+    print(f"\n# exercício 18:\n{frame_filtro_3}")
 
+    # exercício 19
     preco_max = frame['VL_PRECO'].max()
 
     frame_casa_maior_preco = frame[frame['VL_PRECO'] == preco_max]
 
-    print(f"\n{frame_casa_maior_preco['id']}")
+    print(f"\n# exercício 19:\n{frame_casa_maior_preco['id']}")
 
+    # exercício 18
     preco_min = frame['VL_PRECO'].min()
 
     frame_casa_menor_preco = frame[frame['VL_PRECO'] == preco_min]
 
-    print(f"\n{frame_casa_menor_preco['id']}")
+    print(f"\n# exercício 18\n{frame_casa_menor_preco['id']}")
 
-    print(f"\n{frame['DT_REFERENCIA'].min()}")
+    # exercício 19:
+    print(f"\n# exercício 19:\n{frame['DT_REFERENCIA'].min()}")
 
     # exercícios parte 2
 
@@ -162,7 +183,7 @@ if __name__ == '__main__':
 
     print(f"\n{frame.head()}")
 
-    media_preco = pandas.DataFrame({
+    media_preco = pd.DataFrame({
         "id": [x for x, y in enumerate(frame.groupby(["NU_CEP"]))],
         "VL_MEDIA_PRECO": frame.groupby(["NU_CEP"])["VL_PRECO"].mean()
     }).reset_index()
@@ -175,31 +196,32 @@ if __name__ == '__main__':
 
     print(f"\n\nExercícios parte 3\n")
 
+    # exercício 1
     path_cadastro_loja_a = "./Datasets/CadastroLoja_A.xlsx"
     path_cadastro_loja_b = "./Datasets/CadastroLoja_B.xlsx"
     path_tipo_cliente = "./Datasets/TipoCliente.xlsx"
     path_vendas = "./Datasets/Vendas.xlsx"
 
-    data_frame_cadastro_loja_a = pandas.DataFrame(
-        pandas.read_excel(
+    data_frame_cadastro_loja_a = pd.DataFrame(
+        pd.read_excel(
             path_cadastro_loja_a
         )
     )
 
-    data_frame_cadastro_loja_b = pandas.DataFrame(
-        pandas.read_excel(
+    data_frame_cadastro_loja_b = pd.DataFrame(
+        pd.read_excel(
             path_cadastro_loja_b
         )
     )
 
-    data_frame_tipo_cliente = pandas.DataFrame(
-        pandas.read_excel(
+    data_frame_tipo_cliente = pd.DataFrame(
+        pd.read_excel(
             path_tipo_cliente
         )
     )
 
-    data_frame_vendas = pandas.DataFrame(
-        pandas.read_excel(
+    data_frame_vendas = pd.DataFrame(
+        pd.read_excel(
             path_vendas
         )
     )
@@ -209,7 +231,8 @@ if __name__ == '__main__':
     # print(f"\n{data_frame_tipo_cliente}")
     # print(f"\n{data_frame_vendas}")
 
-    print(f"\nLOJA A:\n{data_frame_cadastro_loja_a.head()}")
+    # exercício 2
+    print(f"\n# exercício 2:\nLOJA A:\n{data_frame_cadastro_loja_a.head()}")
     print(f"\n{data_frame_cadastro_loja_a.shape}")
     print(f"\n{data_frame_cadastro_loja_a.dtypes}")
 
@@ -225,22 +248,25 @@ if __name__ == '__main__':
     print(f"\n{data_frame_vendas.shape}")
     print(f"\n{data_frame_vendas.dtypes}")
 
+    # exercício 3
     cliente_loja_a_vendas = data_frame_cadastro_loja_a.merge(
         data_frame_vendas,
         how="inner",
         on="ID"
     )
 
-    print(f"\n{cliente_loja_a_vendas}")
+    print(f"\n# exercício 3\n{cliente_loja_a_vendas}")
 
+    # exercício 4
     cliente_loja_b_vendas = data_frame_cadastro_loja_b.merge(
         data_frame_vendas,
         how="inner",
         on="ID"
     )
 
-    print(f"\n{cliente_loja_b_vendas}")
+    print(f"\n# exercício 4:\n{cliente_loja_b_vendas}")
 
+    # exercício 5 e 6
     cliente_cadastrado_a_b = data_frame_cadastro_loja_a.merge(
         data_frame_cadastro_loja_b,
         how="inner",
@@ -256,8 +282,9 @@ if __name__ == '__main__':
         "TipoCliente_y": "CD_TIPO_CLIENTE_B"
     })
 
-    print(f"\n{cliente_cadastrado_a_b}")
+    print(f"\n# exercício 5 e 6:\n{cliente_cadastrado_a_b}")
 
+    # exercício 7 e 8
     todos_clientes_lojas = data_frame_cadastro_loja_a.merge(
         data_frame_cadastro_loja_b,
         how="outer",
@@ -267,16 +294,18 @@ if __name__ == '__main__':
         subset="ID"
     )
 
-    print(f"\n{todos_clientes_lojas}")
+    print(f"\n# exercício 7 e 8:\n{todos_clientes_lojas}")
 
+    # exercício 9
     soma_gastos_clientes_loja_a = data_frame_cadastro_loja_a.merge(
         data_frame_vendas,
         how="inner",
         on="ID"
     )["Valor"].sum()
 
-    print(f"\n{soma_gastos_clientes_loja_a}")
+    print(f"\n# exercício 9:\n{soma_gastos_clientes_loja_a}")
 
+    # exercício 10
     nome_tipo_cliente_vendas = data_frame_vendas.merge(
         data_frame_cadastro_loja_a.merge(
             data_frame_tipo_cliente,
@@ -312,8 +341,9 @@ if __name__ == '__main__':
         axis=1
     )
 
-    print(f"\n{nome_tipo_cliente_vendas}")
+    print(f"\n# exercício 10:\n{nome_tipo_cliente_vendas}")
 
+    # exercício 11
     clientes_loja_a_nao_compraram = data_frame_cadastro_loja_a.merge(
         data_frame_vendas,
         how="left",
@@ -324,8 +354,9 @@ if __name__ == '__main__':
         clientes_loja_a_nao_compraram["Valor"].isnull()
     ]
 
-    print(f"\n{clientes_loja_a_nao_compraram}")
+    print(f"\n# exercício 11:\n{clientes_loja_a_nao_compraram}")
 
+    # exercício 12
     clientes_loja_b_nao_compraram = data_frame_cadastro_loja_b.merge(
         data_frame_vendas,
         how="left",
@@ -336,8 +367,9 @@ if __name__ == '__main__':
         clientes_loja_b_nao_compraram["Valor"].isnull()
     ]
 
-    print(f"\n{clientes_loja_b_nao_compraram}")
+    print(f"\n# exercício 12:\n{clientes_loja_b_nao_compraram}")
 
+    # exercício 13
     todos_clientes_loja_a_nao_compras = data_frame_vendas.merge(
         data_frame_cadastro_loja_a,
         how="outer",
@@ -358,7 +390,7 @@ if __name__ == '__main__':
         todos_clientes_loja_b_nao_compras["Valor"].isnull()
     ]
 
-    todos_clientes_lojas_nao_compras = pandas.concat([
+    todos_clientes_lojas_nao_compras = pd.concat([
         todos_clientes_loja_a_nao_compras,
         todos_clientes_loja_b_nao_compras
     ]).drop_duplicates(
@@ -368,8 +400,9 @@ if __name__ == '__main__':
         axis=1
     )
 
-    print(f"\n{todos_clientes_lojas_nao_compras}")
+    print(f"\n# exercício 13:\n{todos_clientes_lojas_nao_compras}")
 
+    # exercício 14
     todos_clientes_lojas_compras = todos_clientes_lojas.merge(
         data_frame_vendas,
         how="inner",
@@ -380,14 +413,15 @@ if __name__ == '__main__':
         todos_clientes_lojas_compras["Valor"].notnull()
     ]
 
-    print(f"\n{todos_clientes_lojas_compras}")
+    print(f"\n# exercício 14:\n{todos_clientes_lojas_compras}")
 
+    # exercício 15
     tres_mais_compraram = todos_clientes_lojas_compras.sort_values(
         ["Valor"],
         ascending=False
     )[:3]
 
-    tres_mais_compraram = pandas.DataFrame(
+    tres_mais_compraram = pd.DataFrame(
         tres_mais_compraram,
         columns=[
             "ID",
@@ -400,5 +434,5 @@ if __name__ == '__main__':
         axis=1
     )
 
-    print(f"\n{tres_mais_compraram}")
+    print(f"\n# exercício 15:\n{tres_mais_compraram}")
 
